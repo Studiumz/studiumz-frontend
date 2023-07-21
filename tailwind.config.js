e21ca57fae0425b0ae87072b890af9a7344be20c
michaelsinanta <michaelchristlambert@gmail.com>
2023-07-22 06:59:50 +0700

    prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        violet: '#8056DA'
+        violet: "#8056DA",
       },
       fontSize: {
-        'display-large': '57px',
-        'display-medium': '45px',
-        'display-small': '36px',
-        'headline-large': '32px',
-        'headline-medium': '28px',
-        'headline-small': '24px',
-        'title-large': '22px',
-        'title-medium': '16px',
-        'title-small': '14px',
-        'label-large': '14px',
-        'label-medium': '12px',
-        'label-small': '11px',
-        'body-large': '16px',
-        'body-medium': '14px',
-        'body-small': '12px',
-        'p-md': '16px',
-        'p-sm': '12px',
+        "display-large": "57px",
+        "display-medium": "45px",
+        "display-small": "36px",
+        "headline-large": "32px",
+        "headline-medium": "28px",
+        "headline-small": "24px",
+        "title-large": "22px",
+        "title-medium": "16px",
+        "title-small": "14px",
+        "label-large": "14px",
+        "label-medium": "12px",
+        "label-small": "11px",
+        "body-large": "16px",
+        "body-medium": "14px",
+        "body-small": "12px",
+        "p-md": "16px",
+        "p-sm": "12px",
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [require('flowbite/plugin')],
-}
+  plugins: [require("flowbite/plugin")],
+};

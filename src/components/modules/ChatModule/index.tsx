b4e2357d7ed11c69cee70ef58d916a656e95ece3
risalstr <risa.lestari2002@gmail.com>
2023-07-22 18:49:28 +0700

    add overflow scroll

--- src/components/modules/ChatModule/index.tsx
+++ src/components/modules/ChatModule/index.tsx
@@ -24,7 +24,7 @@ export default function ChatModule() {
               </h5>
             </div>
             <div className="flow-root">
-              <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+              <ul className="divide-y divide-gray-200 dark:divide-gray-700 overflow-y-auto h-56">
                 <li className="py-3 sm:py-4">
                   <div className="flex items-center space-x-4">
                     <div className="shrink-0">
@@ -100,6 +100,58 @@ export default function ChatModule() {
                     </div>
                     <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
                       $67
+                    </div>
+                  </div>
+                </li>
+                <li className="py-3 sm:py-4">
+                  <div className="flex items-center space-x-4">
+                    <div className="shrink-0">
+                      {/* <No
+                        Display
+                        Name
+                        alt="Lana image"
+                        className="rounded-full"
+                        height="32"
+                        src="/images/people/profile-picture-4.jpg"
+                        width="32"
+                      /> */}
+                    </div>
+                    <div className="min-w-0 flex-1">
+                      <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                        Lana Byrd
+                      </p>
+                      <p className="truncate text-sm text-gray-500 dark:text-gray-400">
+                        email@windster.com
+                      </p>
+                    </div>
+                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                      $367
+                    </div>
+                  </div>
+                </li>
+                <li className="py-3 sm:py-4">
+                  <div className="flex items-center space-x-4">
+                    <div className="shrink-0">
+                      {/* <No
+                        Display
+                        Name
+                        alt="Lana image"
+                        className="rounded-full"
+                        height="32"
+                        src="/images/people/profile-picture-4.jpg"
+                        width="32"
+                      /> */}
+                    </div>
+                    <div className="min-w-0 flex-1">
+                      <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                        Lana Byrd
+                      </p>
+                      <p className="truncate text-sm text-gray-500 dark:text-gray-400">
+                        email@windster.com
+                      </p>
+                    </div>
+                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                      $367
                     </div>
                   </div>
                 </li>

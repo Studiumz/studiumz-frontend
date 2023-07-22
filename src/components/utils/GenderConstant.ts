ddb363eb3a3bb02ee1db6a3e3624f39eb2c6bec7
michaelsinanta <michaelchristlambert@gmail.com>
2023-07-23 01:46:11 +0700

    feat: etc

--- src/components/utils/GenderConstant.ts
+++ src/components/utils/GenderConstant.ts
@@ -1,25 +1,24 @@
 const GenderContents = [
-    {
-      subject: "Male",
-      type: "radio",
-      id: "male",
-      value: "male",
-      isRequired: true,
-    },
-    {
-      subject: "Female",
-      type: "radio",
-      id: "female",
-      value: "female",
-      isRequired: true,
-    },
-    {
-      subject: "Others",
-      type: "radio",
-      id: "others",
-      value: "others",
-      isRequired: true,
-    },
-  ];
-  export default GenderContents;
-  
+  {
+    subject: "Male",
+    type: "radio",
+    id: "male",
+    value: "male",
+    isRequired: true,
+  },
+  {
+    subject: "Female",
+    type: "radio",
+    id: "female",
+    value: "female",
+    isRequired: true,
+  },
+  {
+    subject: "Others",
+    type: "radio",
+    id: "others",
+    value: "others",
+    isRequired: true,
+  },
+];
+export default GenderContents;

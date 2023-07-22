6ccfcde02e2a7dea67eb4831e89765a12364d4a8
risalstr <risa.lestari2002@gmail.com>
2023-07-22 09:11:54 +0700

    add studiumz logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { StudiumzLogo } from "@/components/icons/StudiumzLogo";
 import Image from "next/image";
 import { FaGoogle } from "react-icons/fa";
 
@@ -5,15 +6,19 @@ export default function Home() {
   return (
     <>
       <main
-        className={`flex lg:flex-row flex-col z-0 min-h-screen bg-gradient-to-r from-violet to-white opacity-50 items-center justify-center p-24`}
+        className={`flex lg:flex-row flex-col z-0 min-h-screen bg-gradient-to-r  items-center justify-center p-24`}
       >
         <div className="lg:w-1/2 w-full flex flex-col justify-start gap-5">
-          <h1 className="text-violet text-5xl font-extrabold">Studiumz</h1>
-          <h3 className="text-[#808089]">
-            Empowering Senior High Networks: Study, Connect, Succeed.
+          <div className="flex flex-wrap">
+            <StudiumzLogo size={"w-20"} />
+            <h1 className="text-violet text-5xl font-extrabold">Studiumz</h1>
+          </div>
+          <h3 className="text-[#808089] text-2xl">
+            Empowering Senior High Networks: <br />
+            Study, Connect, Succeed.
           </h3>
         </div>
-        <div className="lg:w-1/2 text-violet w-full text-center flex flex-col justify-center items-center gap-5">
+        <div className="lg:w-1/2 text-violet w-full text-center flex flex-col justify-end items-center gap-5">
           <h3>Unlock Your Potential Together.</h3>
           <button className="flex justify-center items-center gap-2 outline outline-offset-2 outline-1 w-48 h-10 rounded-lg decoration-black transform transition-all duration-300 ease-in-out hover:scale-110 hover:opacity-80">
             <svg

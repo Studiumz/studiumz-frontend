b83b0769facfde30b96ab8703f7efdb7262a47c6
risalstr <risa.lestari2002@gmail.com>
2023-07-22 12:02:40 +0700

    update onboarding

--- src/components/elements/modal/index.tsx
+++ src/components/elements/modal/index.tsx
@@ -1,9 +1,64 @@
 import { StudiumzLogo } from "@/components/icons/StudiumzLogo";
+import { Button, Modal as FlowbiteModal } from "flowbite-react";
+import { useState } from "react";
 
-export default function Modal() {
+export default function CustomModal() {
+  const [openModal, setOpenModal] = useState<string | undefined>();
+  const props = { openModal, setOpenModal };
   return (
     <>
-      <div className={`fixed inset-0 flex items-center justify-center z-50`}>
+      {/* <Button onClick={() => props.setOpenModal("default")}>
+        Toggle modal
+      </Button> */}
+      <FlowbiteModal
+      // show={props.openModal === "default"}
+      // onClose={() => props.setOpenModal(undefined)}
+      >
+        <FlowbiteModal.Header>Terms of Service</FlowbiteModal.Header>
+        <FlowbiteModal.Body>{/* Modal content goes here */}</FlowbiteModal.Body>
+        <FlowbiteModal.Footer>
+          <Button onClick={() => props.setOpenModal(undefined)}>
+            I accept
+          </Button>
+          <Button color="gray" onClick={() => props.setOpenModal(undefined)}>
+            Decline
+          </Button>
+        </FlowbiteModal.Footer>
+      </FlowbiteModal>
+      {/* Other JSX content */}
+    </>
+  );
+}
+
+{
+  /* <Modal.Header>Terms of Service</Modal.Header>
+        <Modal.Body>
+          <div className="space-y-6">
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              With less than a month to go before the European Union enacts new
+              consumer privacy laws for its citizens, companies around the world
+              are updating their terms of service agreements to comply.
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              The European Union’s General Data Protection Regulation (G.D.P.R.)
+              goes into effect on May 25 and is meant to ensure a common set of
+              data rights in the European Union. It requires organizations to
+              notify users as soon as possible of high-risk data breaches that
+              could personally affect them.
+            </p>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => props.setOpenModal(undefined)}>
+            I accept
+          </Button>
+          <Button color="gray" onClick={() => props.setOpenModal(undefined)}>
+            Decline
+          </Button>
+        </Modal.Footer> */
+}
+{
+  /* <div className={`fixed inset-0 flex items-center justify-center z-50`}>
         <div className="flex flex-col justify-center items-center gap-5 bg-white w-1/2 p-4 rounded-xl shadow-lg text-center">
           <StudiumzLogo size={"w-24"} />
           <h2 className="text-xl text-violet">Welcome to Studiumz!</h2>
@@ -28,7 +83,5 @@ export default function Modal() {
             Next
           </button>
         </div>
-      </div>
-    </>
-  );
+      </div> */
 }
--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,9 +1,194 @@
-import Modal from "@/components/elements/modal";
+import CustomModal from "@/components/elements/modal";
+import { StudiumzLogo } from "@/components/icons/StudiumzLogo";
+import { Label, TextInput, Button } from "flowbite-react";
 
 export default function Onboarding() {
   return (
     <>
-      <Modal />
+      <CustomModal />
+
+      <form className="flex flex-col p-5 justify-center items-center">
+        <StudiumzLogo size={"w-20"} className="my-10" />
+
+        <div className="flex lg:flex-row flex-col gap-16">
+          <div className="w-1/2">
+            <div className="mb-6">
+              <div className="mb-2 block">
+                <Label htmlFor="email1" value="Your email" />
+              </div>
+              <TextInput
+                id="email1"
+                placeholder="name@gmail.com"
+                required
+                type="email"
+              />
+            </div>
+            <div className="mb-6">
+              <div className="mb-2 block">
+                <Label htmlFor="name" value="Full Name" />
+              </div>
+              <TextInput id="name" sizing="md" type="text" />
+            </div>
+            <div className="mb-6">
+              <div className="mb-2 block">
+                <Label htmlFor="nickname" value="Nickname" />
+              </div>
+              <TextInput id="nickname" sizing="md" type="text" />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="text"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Gender
+              </label>
+              <ul className="flex flex-wrap gap-2 w-full">
+                <li>
+                  <input
+                    type="radio"
+                    id="male"
+                    name="hosting"
+                    value="male"
+                    className="hidden peer"
+                    required
+                  />
+                  <label
+                    htmlFor="male"
+                    className="inline-flex items-center justify-center w-32 py-1 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-violet peer-checked:text-violet hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+                  >
+                    <div className="block">Male</div>
+                  </label>
+                </li>
+                <li>
+                  <input
+                    type="radio"
+                    id="female"
+                    name="hosting"
+                    value="female"
+                    className="hidden peer"
+                  />
+                  <label
+                    htmlFor="female"
+                    className="inline-flex items-center justify-center w-32 py-1 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-violet peer-checked:text-violet hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+                  >
+                    <div className="block">Female</div>
+                  </label>
+                </li>
+                <li>
+                  <input
+                    type="radio"
+                    id="others"
+                    name="hosting"
+                    value="others"
+                    className="hidden peer"
+                  />
+                  <label
+                    htmlFor="others"
+                    className="inline-flex items-center justify-center w-32 py-1 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-violet peer-checked:text-violet hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+                  >
+                    <div className="block">Others</div>
+                  </label>
+                </li>
+              </ul>
+            </div>
+
+            <div className="mb-6">
+              <label
+                htmlFor="text"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Date of birth
+              </label>
+              <ul>
+                <li>
+                  <TextInput
+                    // {...register("start_date", {
+                    //   required: false,
+                    //   valueAsDate: true,
+                    //   validate: (val) =>
+                    //     val === new Date(defaultData.start_date) ||
+                    //     val < control._formValues.end_date ||
+                    //     "Tanggal & waktu mulai harus sebelum tanggal & waktu selesai.",
+                    // })}
+                    type="datetime-local"
+                  />
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="stories" value="Share your struggle stories" />
+            </div>
+            <TextInput id="stories" sizing="lg" type="text" />
+
+            <div className="my-10">
+              <label
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Choose your interests
+              </label>
+              <ul className="flex gap-3">
+                <li>
+                  <input
+                    type="radio"
+                    id="biologi"
+                    name="hosting"
+                    value="biologi"
+                    className="hidden peer"
+                    required
+                  />
+                  <label
+                    htmlFor="biologi"
+                    className="inline-flex items-center justify-center w-32 py-1 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-violet peer-checked:text-violet hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+                  >
+                    <div className="block">Biologi</div>
+                  </label>
+                </li>
+                <li>
+                  <input
+                    type="radio"
+                    id="kimia"
+                    name="hosting"
+                    value="kimia"
+                    className="hidden peer"
+                    required
+                  />
+                  <label
+                    htmlFor="kimia"
+                    className="inline-flex items-center justify-center w-32 py-1 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-violet peer-checked:text-violet hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+                  >
+                    <div className="block">Kimia</div>
+                  </label>
+                </li>
+                <li>
+                  <input
+                    type="radio"
+                    id="fisika"
+                    name="hosting"
+                    value="fisika"
+                    className="hidden peer"
+                    required
+                  />
+                  <label
+                    htmlFor="fisika"
+                    className="inline-flex items-center justify-center w-32 py-1 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-violet peer-checked:text-violet hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+                  >
+                    <div className="block">Fisika</div>
+                  </label>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <Button type="submit" className="cursor-pointer bg-violet px-3 my-5">
+          Submit
+        </Button>
+      </form>
+      {/* <Modal /> */}
     </>
   );
 }
